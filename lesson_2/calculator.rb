1aa8c669df5add693bcc4c943d466fee08c85cf1
John Finlay <finlay_john@hotmail.com>
2020-06-29 06:55:22 -0500

    validate operator, grab user name

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -6,13 +6,20 @@ def valid_number?(num)
   num.to_i != 0
 end
 
-prompt("Welcome to Calculator!")
+prompt("Welcome to Calculator! Enter your name:")
+name = ''
+loop do
+  name = gets.chomp
+  if name.empty?
+    prompt("Make sure to use a valid name.")
+  else
+    break
+  end
+end
+prompt("Hi #{name}!")
 
 loop do
   number1 = ''
-  number2 = ''
-  operator = ''
-
   loop do
     prompt("What's the first number?")
     number1 = gets.chomp
@@ -23,7 +30,7 @@ loop do
       prompt("Hmm... that doesn't look like a valid number")
     end
   end
-
+  number2 = ''
   loop do
     prompt("What's the second number?")
     number2 = gets.chomp
@@ -37,8 +44,16 @@ loop do
 
   prompt("What operation would you like to perform? 1) add 2) subtract
     3) multiply 4) divide")
-  operator = gets.chomp
 
+  operator = ''
+  loop do
+    operator = gets.chomp
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt("Must choose 1, 2, 3 or 4")
+    end
+  end
   result =  case operator
             when '1'
               number1.to_i + number2.to_i
