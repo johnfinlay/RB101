234b9c07cf96ca84c01867015d6d3cce301736b3
John Finlay <finlay_john@hotmail.com>
2020-06-29 06:46:38 -0500

    add loop to continue/quit

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -6,47 +6,53 @@ def valid_number?(num)
   num.to_i != 0
 end
 
-
 prompt("Welcome to Calculator!")
-number1 = ''
-number2 = ''
-operator = ''
 
 loop do
-  prompt("What's the first number?")
-  number1 = gets.chomp
+  number1 = ''
+  number2 = ''
+  operator = ''
 
-  if valid_number?(number1)
-    break
-  else
-    prompt("Hmm... that doesn't look like a valid number")
+  loop do
+    prompt("What's the first number?")
+    number1 = gets.chomp
+
+    if valid_number?(number1)
+      break
+    else
+      prompt("Hmm... that doesn't look like a valid number")
+    end
   end
-end
 
-loop do
-  prompt("What's the second number?")
-  number2 = gets.chomp
+  loop do
+    prompt("What's the second number?")
+    number2 = gets.chomp
 
-  if valid_number?(number2)
-    break
-  else
-    prompt "Hmm... that doesn't look like a valid number"
+    if valid_number?(number2)
+      break
+    else
+      prompt "Hmm... that doesn't look like a valid number"
+    end
   end
+
+  prompt("What operation would you like to perform? 1) add 2) subtract
+    3) multiply 4) divide")
+  operator = gets.chomp
+
+  result =  case operator
+            when '1'
+              number1.to_i + number2.to_i
+            when '2'
+              number1.to_i - number2.to_i
+            when '3'
+              number1.to_i * number2.to_i
+            when '4'
+              number1.to_f / number2.to_f
+            end
+
+  prompt("The result is #{result}")
+
+  prompt("Do you want to perform another calculation? (Y to calculate again)")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
 end
-
-prompt("What operation would you like to perform? 1) add 2) subtract
-  3) multiply 4) divide")
-operator = gets.chomp
-
-result =  case operator
-          when '1'
-            number1.to_i + number2.to_i
-          when '2'
-            number1.to_i - number2.to_i
-          when '3'
-            number1.to_i * number2.to_i
-          when '4'
-            number1.to_f / number2.to_f
-          end
-
-prompt("The result is #{result}")
