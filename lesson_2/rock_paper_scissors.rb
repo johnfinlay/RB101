405aaa68330b3ebe9b364447b928d8d9c0d414f3
John Finlay <finlay_john@hotmail.com>
2020-07-09 07:27:15 -0500

    add score keeping to rps

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -13,40 +13,82 @@ WINNERS = {
   'lizard' => ['spock', 'paper'],
   'spock' => ['scissors', 'rock']
 }
+
 def win?(first, second)
   WINNERS[VALID_CHOICES[first]].include?(VALID_CHOICES[second])
 end
 
-def display_results(player, computer)
+def display_results(result)
+  case result
+  when 'player' then prompt("You won!")
+  when 'computer' then prompt("Computer won!")
+  else prompt("It's a tie!")
+  end
+end
+
+def get_result(player, computer)
   if win?(player, computer)
-    prompt("You won!")
+    'player'
   elsif win?(computer, player)
-    prompt("Computer won!")
+    'computer'
   else
-    prompt("It's a tie!")
+    'tie'
+  end
+end
+
+def update_score(players, result)
+  case result
+  when 'player' then players[:player][:score] += 1
+  when 'computer' then players[:computer][:score] += 1
   end
 end
 
 def prompt(message)
   puts "=> #{message}"
 end
-
+players = {
+  computer: {
+    choice: '',
+    score: 0
+  },
+  player: {
+    choice: '',
+    score: 0
+  }
+}
 loop do
-  choice = ''
+  players[:computer][:score] = 0
+  players[:player][:score] = 0
+
   loop do
-    prompt("Choose one: rock(r), paper(p), scissors(sc), lizard(l), spock(sp)")
-    choice = gets.chomp
+    loop do
+      prompt("Choose one: rock(r), paper(p), "\
+        "scissors(sc), lizard(l), spock(sp)")
+      players[:player][:choice] = gets.chomp
 
-    break if VALID_CHOICES.keys.include?(choice)
-    prompt("That's not a valid choice.")
+      break if VALID_CHOICES.keys.include?(players[:player][:choice])
+      prompt("That's not a valid choice.")
+    end
+
+    players[:computer][:choice] = VALID_CHOICES.keys.sample
+
+    prompt("You chose: #{VALID_CHOICES[players[:player][:choice]]}; "\
+      "Computer chose: #{VALID_CHOICES[players[:computer][:choice]]}")
+    result = get_result(players[:player][:choice], players[:computer][:choice])
+    display_results(result)
+    update_score(players, result)
+
+    if players[:computer][:score] == 5
+      prompt("Computer won the match!")
+      break
+    elsif players[:player][:score] == 5
+      prompt("You won the match!")
+      break
+    else
+      prompt("The score is now you: #{players[:player][:score]}, "\
+        "computer: #{players[:computer][:score]}\n\n")
+    end
   end
-
-  computer_choice = VALID_CHOICES.keys.sample
-
-  prompt("You chose: #{VALID_CHOICES[choice]}; "\
-    "Computer chose: #{VALID_CHOICES[computer_choice]}")
-
-  display_results(choice, computer_choice)
 
   prompt("Would you like to play again?")
   answer = gets.chomp
