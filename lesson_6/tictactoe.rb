0aabe8a597100e44170c05e6cb721f86be4e5ea5
John Finlay <finlay_john@hotmail.com>
2020-07-20 07:27:09 -0500

    tictactoe determine winner

--- lesson_6/tictactoe.rb
+++ lesson_6/tictactoe.rb
@@ -56,15 +56,41 @@ def board_full?(brd)
 end
 
 def winner?(brd)
-  false
+  !!detect_winner(brd)
+end
+
+def detect_winner(brd)
+  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
+                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] +
+                  [[1, 5, 9], [3, 5, 7]]
+  winning_lines.each do |line|
+    if brd[line[0]] == PLAYER_MARKER &&
+       brd[line[1]] == PLAYER_MARKER &&
+       brd[line[2]] == PLAYER_MARKER
+      return 'Player'
+    elsif brd[line[0]] == COMPUTER_MARKER &&
+          brd[line[1]] == COMPUTER_MARKER &&
+          brd[line[2]] == COMPUTER_MARKER
+      return 'Computer'
+    end
+  end
+  nil
 end
 
 board = initialize_board
-display_board(board)
 
 loop do
-  player_chooses!(board)
-  computer_chooses!(board)
   display_board(board)
+  player_chooses!(board)
   break if winner?(board) || board_full?(board)
+  computer_chooses!(board)
+  break if winner?(board) || board_full?(board)
+end
+
+display_board(board)
+
+if winner?(board)
+  prompt "#{detect_winner(board)} won!"
+else
+  prompt "It's a tie!"
 end
