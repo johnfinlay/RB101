03b83f5855173ce57efa279a6493ea58b7288105
John Finlay <finlay_john@hotmail.com>
2020-07-20 06:25:15 -0500

    tictactoe player choice

--- lesson_6/tictactoe.rb
+++ lesson_6/tictactoe.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
+
+def prompt(msg)
+  puts "=> #{msg}"
+end
+
 def display_board(brd)
   puts ""
   puts "     |     |"
@@ -18,9 +26,27 @@ end
 
 def initialize_board
   new_board = {}
-  (1..9).each { |num| new_board[num] = ' ' }
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
 end
 
+def empty_squares(brd)
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
+end
+
+def player_chooses!(brd)
+  square = ''
+  loop do
+    prompt "Choose a square (#{empty_squares(brd).join(',')}):"
+    square = gets.chomp.to_i
+    break if empty_squares(brd).include?(square)
+    prompt "Sorry, that's not a valid choice."
+  end
+  brd[square] = PLAYER_MARKER
+end
+
 board = initialize_board
+display_board(board)
+player_chooses!(board)
+puts board.inspect
 display_board(board)
