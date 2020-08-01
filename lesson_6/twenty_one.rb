df814e4bca1ae4592e957cd4e5f4361adaf0622a
John Finlay <finlay_john@hotmail.com>
2020-08-01 08:28:48 -0500

    21 display hands

--- lesson_6/twenty_one.rb
+++ lesson_6/twenty_one.rb
@@ -3,6 +3,10 @@ require 'pry'
 CARD_SUITS = ['H', 'S', 'D', 'C']
 CARD_VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
+def prompt(msg)
+  puts "=> #{msg}"
+end
+
 def new_deck
   cards = []
   CARD_SUITS.each do |suit|
@@ -17,19 +21,52 @@ def deal_card!(hand, deck)
   hand << deck.shift
 end
 
+def format_cards(cards)
+  list = ""
+  cards.each { |card| list << card[1] + card[0] + ' ' }
+  list.strip
+end
+
 def display_hands(hands, hide_one = true)
+  dealer_cards = hands[:dealer].map.with_index do |card, index|
+    index == 0 && hide_one ? ['?', '?'] : card
+  end
+  prompt "Dealer Cards:"
+  prompt format_cards(dealer_cards)
+  prompt "Player Cards:"
+  prompt format_cards(hands[:player])
+end
+
+def player_turn(hands)
 
 end
 
-def player_turn
+def total(cards)
+  values = cards.map { |card| card[1] }
+
+  sum = 0
+  values.each do |value|
+    if value == 'A'
+      sum += 11
+    elsif value.to_i == 0
+      sum += 10
+    else
+      sum += value.to_i
+    end
+  end
+
+  values.select { |value| value == 'A' }.count.times do
+    sum -= 10 if sum > 21
+  end
+
+  sum
+end
+
+def dealer_turn(cards)
 
 end
 
-def dealer_turn
-
-end
-
-def busted?(hand)
+def busted?(cards)
 
 end
 
@@ -53,6 +90,6 @@ deck = new_deck
   deal_card!(hands[:player], deck)
 end
 
-binding.pry
 
-p hands
+display_hands(hands, true)
+display_hands(hands, false)
